8dcfae92000207b1a00a7f78fd046ee4361f9a1c
Sthuthi <sthuthisadananda@gmail.com>
2018-08-08 23:13:33 +0530

    ...

--- app/src/main/java/com/dandd/kitapp/page6.java
+++ app/src/main/java/com/dandd/kitapp/page6.java
@@ -2,12 +2,25 @@ package com.dandd.kitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class page6 extends AppCompatActivity {
-
+    ListView listview;
+    List list = new ArrayList();
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page6);
+        listview =(ListView) findViewById(R.id.lists);
+
+
+
     }
 }
