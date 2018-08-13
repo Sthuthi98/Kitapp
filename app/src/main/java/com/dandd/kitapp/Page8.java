6cf6391e6816ed852ee4bc7d8bb79af503dac316
nasil saniah <nasilsaniah98@gmail.com>
2018-08-13 17:29:41 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/drawable/round_shape.xml
    #	app/src/main/res/layout/activity_page6.xml

--- app/src/main/java/com/dandd/kitapp/Page8.java
+++ app/src/main/java/com/dandd/kitapp/Page8.java
@@ -1,9 +1,15 @@
 package com.dandd.kitapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Page8 extends AppCompatActivity {
+    final Context context = this;
+    private EditText ipAddress;
+    private Button up, down,right,left;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/dandd/kitapp/SplashScreen.java
+++ app/src/main/java/com/dandd/kitapp/SplashScreen.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+        for(int i=0;i<=1000;i++);
         finish();
     }
 }
