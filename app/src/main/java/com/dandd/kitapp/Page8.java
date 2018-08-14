f400bf265dab8af87a3ef2d93a2ffb89e1e06089
nasil saniah <nasilsaniah98@gmail.com>
2018-08-14 18:01:00 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/drawable/round_shape.xml
    #	app/src/main/res/layout/activity_page6.xml

--- app/src/main/java/com/dandd/kitapp/Page8.java
+++ app/src/main/java/com/dandd/kitapp/Page8.java
@@ -3,17 +3,28 @@ package com.dandd.kitapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Page8 extends AppCompatActivity {
-    final Context context = this;
-    private EditText ipAddress;
-    private Button up, down,right,left;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page8);
+    }
+    public void up (View view){
+
+    }
+    public void down (View view){
+
+    }
+    public void right(View view){
+
+    }
+    public void left(View view){
+
     }
 }
